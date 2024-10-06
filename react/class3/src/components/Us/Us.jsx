97055d9cc6d9f4ff6c4c8b7f62b67090b4b4c8e8
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-06 10:49:57 +0500

    object using state

--- react/class3/src/components/Us/Us.jsx
+++ react/class3/src/components/Us/Us.jsx
@@ -1,33 +1,57 @@
 import React, { useState } from 'react'
- 
+
 
 export default function Us() {
-    const ab=()=>{
-        console.log('rerendring')
-        return 8
-    }
+    // const ab = () => {
+    //     console.log('rerendring')
+    //     return 8
+    // }
     // const [num, setNum] = useState(()=>{
     //     console.log('rerendring')
     //     return 8
     // })
-    const [num ,setNum]=useState(()=>{ return 9 } )
-    const decrement = () => {
-        setNum(abc => abc - 1)
-        
-        // setNum(abc => abc - 1)
-        // setNum((abc)=>{
-        //     return abc -1
+    // const [num ,setNum]=useState(()=>{ return 9 } )
+    // const decrement = () => {
+    // setNum(abc => abc - 1)
+
+    // setNum(abc => abc - 1)
+    // setNum((abc)=>{
+    //     return abc -1
+    // })
+    // }
+    // const incerement = () => {
+    //     setNum(abc => abc + 1)
+    // }
+    const [num, setNum] = useState({ name: 'eesha', rollno: 12 })
+    const [i, setI] = useState(0)
+
+    const dec = () => {
+        // setNum((preValue) => {
+        //     return {
+        //         ...preValue,
+        //         name: preValue.name = 'amara',
+        //         rollno: preValue.rollno + 1
+        //     }
         // })
+        const names = ['a', 'b', 'c', 'd','e']
+
+        setNum((preValue) => (
+            {
+                ...preValue,
+                name: preValue.name = names[i],
+                rollno: preValue.rollno + 1,
+
+            }))
+        setI(prev => prev + 1)
     }
-    const incerement = () => {
-        setNum(abc => abc + 1)
-    }
+
     return (
         <div>
             <div className='flex gap-2'>
-                <div onClick={decrement}>-</div>
-                <div>{num}</div>
-                <div onClick={incerement}>+</div>
+                <div onClick={dec}>-</div>
+                <div>{num.name}</div>
+                <div>{num.rollno}</div>
+                <div >+</div>
             </div>
 
         </div>
