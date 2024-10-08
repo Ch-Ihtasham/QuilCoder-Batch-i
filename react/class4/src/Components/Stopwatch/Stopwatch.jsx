45649c2256e8c4d0bfe583d9ecadc9c1ab70ad8f
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-08 21:30:03 +0500

    stop watch

--- react/class4/src/Components/Stopwatch/Stopwatch.jsx
+++ react/class4/src/Components/Stopwatch/Stopwatch.jsx
@@ -20,14 +20,20 @@ export default function Stopwatch() {
         clearInterval(a.current)
     }
     time = (endTime - startTime) / 1000
+    const [abc, setAbc] = useState(true)
 
 
     return (
         <div className='w-5/6 m-auto'>
-            <div><span className='bg-white text-black  text-center my-10 font-semibold text-xl p-2'>time is</span>
+            <div className='mb-4'><span className='bg-white text-black  text-center my-10 font-semibold text-xl p-2'>time is</span>
                 <span>{time.toFixed(3)}</span></div>
             <button onClick={start} className='bg-white text-black font-semibold px-6 py-1 rounded-full mr-4 border-4 border-black '>start</button>
             <button onClick={stop} className='bg-red-700 text-white font-semibold px-6 py-1 rounded-full mr-4 border-4 border-white'>stop</button>
+
+            <button onClick={() => {
+                abc ? start() : stop(),
+                    setAbc(prevalue => !prevalue)
+            }} >{abc ? 'start' : 'stop'}</button>
 
         </div>
     )
