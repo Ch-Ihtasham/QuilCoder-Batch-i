4a67c34bf17176ab02c863f69ad4e5d8914ebf40
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-09 21:20:26 +0500

    commit 2

--- react/class4/src/Components/Class5/Class5.jsx
+++ react/class4/src/Components/Class5/Class5.jsx
@@ -4,14 +4,14 @@ export default function Class5() {
     const inp = useRef(null)
     return (
         <div>
-            <div className='text-xl hover:text-green-500 '>
+            <div className='text-xl hover:text-green-500 transition duration-1000'>
                 this is our class five
             </div>
             <input ref={inp} type="text" />
             <button className='bg-white text-black dark:bg-black dark:text-white' onClick={() => inp.current.focus()} >focus btn</button>
 
-            <div className='grid'>
-                <div className=' bg-red-300 w-96'>1</div>
+            <div className='grid grid-cols-3 gap-3'>
+                <div className=' bg-red-300 w-96 border '>1</div>
                 <div className=' bg-red-300 w-96'>2</div>
                 <div className=' bg-red-300 w-96'>3</div>
                 <div className=' bg-red-300 w-48'>4</div>
@@ -19,6 +19,7 @@ export default function Class5() {
                 <div className=' bg-red-300 w-48'>6</div>
                 <div className=' bg-red-300 w-48'>7</div>
             </div>
+            <div className=' relative' ><button className='absolute translate-x-0 hover:translate-x-[400px] transition duration-1000'>hover me</button></div>
         </div>
     )
 }
