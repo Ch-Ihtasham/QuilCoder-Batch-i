3f31fa70ca80770584ca79110d8eb0b0c01b7a87
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-10 20:23:11 +0500

    class6 end

--- react/class4/src/Components/Class6/Class6.jsx
+++ react/class4/src/Components/Class6/Class6.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Class6() {
-  return (
-    <div>
-      this is class 6
-    </div>
-  )
+    
+    const [btn, setBtn] = useState(true)
+    return (
+        <div>
+            {/* {
+                btn ? <div>this is quilcoders batch one</div> : <div> something went wrong</div>
+            } */}
+            {
+                btn && <div>eesha like fast food </div>
+            }
+
+            <button onClick={() => setBtn(v => !v)} >true</button>
+
+        </div>
+    )
 }
+
--- react/class4/src/main.jsx
+++ react/class4/src/main.jsx
@@ -4,13 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import Stopwatch from './Components/Stopwatch/Stopwatch.jsx'
 import Class5 from './Components/Class5/Class5.jsx'
-
+import Class6 from './Components/Class6/Class6.jsx'
 
 createRoot(document.getElementById('root')).render(
   <div>
     {/* <App /> */}
     {/* <Stopwatch /> */}
     {/* <Class5 /> */}
+    <Class6 />
 
   </div>
 )
