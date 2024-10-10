9bdd99ec11cde9803f5c507eabf8140fe4d04c0a
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-10 20:39:24 +0500

    class 7

--- react/class4/src/Components/Class7/Class7.jsx
+++ react/class4/src/Components/Class7/Class7.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
 export default function Class7() {
+    function Post() {
+        return (
+            <button className='bg-blue-500 px-2 py-1 font-semibold border border-white rounded-full'>Post</button>
+        )
+    }
+    const Play = () => {
+        return (
+            <button className='bg-blue-500 px-2 py-1 font-semibold border border-white rounded-full'>Play</button>
+        )
+    }
     return (
         <div>
+            <button className='bg-blue-500 px-2 py-1 font-semibold border border-white rounded-full'>Connect</button>
+            <Follow />
+            <Play />
 
+            <Post />
+            {/* <Eesha/> */}
         </div>
+    )
+}
+
+const Follow = () => {
+    return (
+        <button className='bg-blue-500 px-2 py-1 font-semibold border border-white rounded-full'>Follow</button>
+    )
+}
+
+function Eesha() {
+    return (
+        <div>this is eeshas function</div>
     )
 }
--- react/class4/src/main.jsx
+++ react/class4/src/main.jsx
@@ -5,13 +5,15 @@ import './index.css'
 import Stopwatch from './Components/Stopwatch/Stopwatch.jsx'
 import Class5 from './Components/Class5/Class5.jsx'
 import Class6 from './Components/Class6/Class6.jsx'
+import Class7 from './Components/Class7/Class7.jsx'
 
 createRoot(document.getElementById('root')).render(
   <div>
     {/* <App /> */}
     {/* <Stopwatch /> */}
     {/* <Class5 /> */}
-    <Class6 />
+    {/* <Class6 /> */}
+    <Class7 />
 
   </div>
 )
