5122da75b447e3c4d902e016b0ce4bb2666ddd03
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-12 10:31:46 +0500

    commit

--- react/class4/src/Components/Forms/Forms.jsx
+++ react/class4/src/Components/Forms/Forms.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Forms() {
+    const [form, setForm] = useState({
+        firstName: '',
+        lastName: '',
+        Phone: '',
+        email: ''
+    })
+    function handleChange(e) {
+        const { name, value } = e.target
+        setForm({
+            ...form,
+            [name]: value
+        })
+    }
+    function dataJaRahaHa(e) {
+        e.preventDefault()
+        console.log(form)
+    }
+
+
     return (
         <div>
+            <form onSubmit={dataJaRahaHa}>
+                <div>
+                    <label htmlFor="">Name:</label>
+                    <input type="text"
+                        name='firstName'
+                        onChange={handleChange}
+                        value={form.firstName}
+                    /> <br /> <br />
+                    <label htmlFor="">LastName</label>
+                    <input type="text"
+                        name='lastName'
+                        onChange={handleChange}
+                        value={form.lastName}
+                    /> <br /> <br />
+                    <label htmlFor="">Phone</label>
+                    <input type="no"
+                        name='Phone'
+                        onChange={handleChange}
+                        value={form.Phone}
+                    /> <br /> <br />
+                    <label htmlFor="">Email</label>
+                    <input type="email"
+                        name='email'
+                        onChange={handleChange}
+                        value={form.email}
+                    />
+                    <button type='submit' >send data</button>
+
+
+                </div>
+            </form>
+            <fieldset style={{border:'2px solid black'}}>
+                <legend>Personal Information</legend>
+                <label htmlFor="firstName">First Name:</label>
+                <input type="text" id="firstName" name="firstName" />
+                <br /><br />
+                <label htmlFor="lastName">Last Name:</label>
+                <input type="text" id="lastName" name="lastName" />
+            </fieldset>
 
         </div>
     )
--- react/class4/src/main.jsx
+++ react/class4/src/main.jsx
@@ -9,6 +9,7 @@ import Class7 from './Components/Class7/Class7.jsx'
 import { Post, Follow, Play, Connect } from './Components/Btn/Btn.jsx'
 // import SimpleSlider from './Components/slider/CustomArrows.jsx'
 import CustomArrows from './Components/slider/Slider.jsx'
+import Forms from './Components/Forms/Forms.jsx'
 
 createRoot(document.getElementById('root')).render(
   <div>
@@ -24,8 +25,9 @@ createRoot(document.getElementById('root')).render(
     <Connect />
     <Play />
     <Follow /> */}
-    <div className='w-5/6 m-auto'><CustomArrows /></div>
+    {/* <div className='w-5/6 m-auto'><CustomArrows /></div> */}
 
+    <Forms />
 
 
   </div>
