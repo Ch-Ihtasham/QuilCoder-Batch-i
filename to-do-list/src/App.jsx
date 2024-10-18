df87b54eeecd2f9b918d7adc26f8a0fb724c7654
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-18 23:17:13 +0500

    commit

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,34 +1,75 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newtask, setNewTask] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [isEdit, setIsEdit] = useState(null)
+  const [newEdit, setNewEdit] = useState('')
+  function handelAdd() {
+    setTasks([...tasks, { toDo: newtask, isCheck: false }])
+    setNewTask('')
+
+  }
+  function handelCheck(index) {
+    const updateCheckTask = tasks.map((v, i) =>
+      i === index ? { ...v, isCheck: !v.isCheck } : v
+    )
+
+    setTasks(updateCheckTask)
+  }
+  function handelEdit(index) {
+    setIsEdit(index)
+    setNewEdit(tasks[index].toDo)
+  }
+  function handelSave(index) {
+    const updateCheckTask = tasks.map((v, i) =>
+      i === index ? { ...v, toDo: newEdit } : v
+    )
+
+    setTasks(updateCheckTask)
+    setIsEdit(null)
+  }
+  function handelRemove(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index)
+    setTasks(updatedTasks)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-3/4 m-auto'>
+      <div className='text-center '>
+        <h1 className='text-4xl font-bold my-4'>Our Todo List</h1>
+        <div >
+          <input type="text" onChange={(e) => setNewTask(e.target.value)} value={newtask} className='border-2 border-black' />
+          <button onClick={handelAdd} className='bg-black text-white px-2 ml-3'>Add</button>
+        </div>
+        <div className='text-center my-4'>
+          <h1 className='text-3xl font-semibold'>Our Task To Do</h1>
+          {tasks.map((task, index) => (
+            <div className='flex gap-4 justify-center items-center my-3' key={index}>
+              <input type="checkbox" onChange={() => handelCheck(index)} checked={task.isCheck} />
+              {isEdit === index ? (
+                <input className='border-2 border-black' type='text' value={newEdit} onChange={(e) => setNewEdit(e.target.value)} />
+              ) : (
+                <div className={task.isCheck ? 'line-through' : ''}>{task.toDo}</div>
+              )
+
+              }
+              <div className='flex gap-4'>
+                {isEdit === index ? (
+                  <button onClick={() => handelSave(index)}>Save</button>
+                ) : (
+                  <button onClick={() => handelEdit(index)}>Edit</button>
+                )}
+                <button onClick={() => handelRemove(index)}>Remove</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
