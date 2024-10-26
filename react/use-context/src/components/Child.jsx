5b38a2caa79f4e76915f9972b6a65a92234a134a
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-26 10:30:38 +0500

    commit

--- react/use-context/src/components/Child.jsx
+++ react/use-context/src/components/Child.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SubChild from './SubChild'
-
+import { useContext } from 'react'
+import { Counts } from '../context/Context'
 export default function Child() {
+    const data = useContext(Counts)
     return (
         <div>
-            <SubChild/>
-            this is first child
+            <SubChild />
+            this is first child{data}
         </div>
     )
 }
