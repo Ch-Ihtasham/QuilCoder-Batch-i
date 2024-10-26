fd25c85dade1466119a3eb891c50fca13a685677
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-10-26 09:35:42 +0500

    commit

--- react/routess/src/pages/Github.jsx
+++ react/routess/src/pages/Github.jsx
@@ -2,11 +2,25 @@ import React, { useEffect, useState } from 'react'
 
 export default function Github() {
     const [data, setData] = useState('')
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/ch-ihtasham')
+    //         .then(respone => respone.json())
+    //         .then(data => setData(data))
+    // }, [])
     useEffect(() => {
-        fetch('https://api.github.com/users/ch-ihtasham')
-            .then(respone => respone.json())
-            .then(data => setData(data))
+        async function gitdata() {
+            try {
+                const respone = await fetch('https://api.github.com/users/ch-ihtasham')
+                const outcome = await respone.json()
+                setData(outcome)
+            }
+            catch (error) {
+                console.error(error)
+            }
+        }
+        gitdata()
     }, [])
+
     return (
         <div>
             this page belongs to Github
