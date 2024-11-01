7bcac40a8c047005ddb3da715a9e83828daaad3a
Ch Ihtasham <143878087+Ch-Ihtasham@users.noreply.github.com>
2024-11-01 20:35:27 +0500

    commit

--- react/use-context/src/App.jsx
+++ react/use-context/src/App.jsx
@@ -1,49 +1,52 @@
-import { useMemo, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Child from './components/Child'
 import { Counts } from './context/Context'
-const num = new Array(30_000_000).fill(0).map((v, i) => {
-  return {
-    index: i,
-    ismagical: i === 29_000_000
-  }
-})
+// const num = new Array(30_000_000).fill(0).map((v, i) => {
+//   return {
+//     index: i,
+//     ismagical: i === 29_000_000
+//   }
+// })
 
 
 
 
 function App() {
   const [count, setCount] = useState(0)
-  const [number, setNumber] = useState(num)
-  const ans = useMemo(() => number.find((item) => item.ismagical),[])
+  // const [number, setNumber] = useState(num)
+  // const ans = useMemo(() => number.find((item) => item.ismagical),[])
+  const amara = useCallback(() => {
+    return 'hello world'
+  },[])
   return (
     <>
-      <Counts.Provider value={count}>
-        <Child />
-        {ans.index}
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
+      {/* <Counts.Provider value={count}> */}
+      <Child count={1} fun={amara} />
+      {/* {ans.index}  */}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </Counts.Provider>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      {/* </Counts.Provider> */}
     </>
   )
 }
--- react/use-context/src/components/Child.jsx
+++ react/use-context/src/components/Child.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { memo } from 'react'
 import SubChild from './SubChild'
 import { useContext } from 'react'
 import { Counts } from '../context/Context'
-export default function Child() {
+export default memo(function Child({ count }) {
     const data = useContext(Counts)
+    console.log('chid is rendering')
     return (
         <div>
             <SubChild />
-            this is first child{data}
+            this is out child which not rerending{count}
+            {/* this is first child{data} */}
         </div>
     )
-}
+})
