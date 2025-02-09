88fc9b9c9046674e6360c17b2211a826c2624f4b
Ch Ihtasham <cihtasham32@gmail.com>
2025-02-10 01:05:12 +0500

    weather app

--- react/weather-app/src/App.js
+++ react/weather-app/src/App.js
@@ -6,12 +6,28 @@ function App() {
   const [weather, setWeather] = useState(null)
   const apiKey = '5a5b9db67354136d5ab2c73981bb2ded'
 
-  const featchWeather = async () => {
-    if (!city) {
-      alert('plese enter city name')
+  const featchWeather = async (lat, long) => {
+    let url;
+    // if (!city) {
+    //   alert('plese enter city name')
+    //   return;
+    // }
+    if (city) {
+      url = (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+
     }
+    else if (lat && long) {
+      url = (`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${long}&appid=${apiKey}`)
+      setCity('')
+    }
+
+    else {
+      alert("Please enter a city name or allow location access.");
+      return;
+    }
+
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+      const response = await fetch(url)
       if (!response.ok) {
         throw new Error('the city data  is not avaliable')
       }
@@ -20,32 +36,78 @@ function App() {
       setWeather(data)
     }
     catch (amara) {
-      
       console.log(amara)
     }
   }
+  function location() {
+    setCity('')
+    if (navigator.geolocation) {
+      
+      navigator.geolocation.getCurrentPosition(
+        amara => {
+          let lat = amara.coords.latitude;
+          let long = amara.coords.longitude
+          console.log(lat, long)
+          setCity('')
+          featchWeather(lat, long)
+          
+        },
+        error => {
+
+          alert('Unable to retrieve your location. Please allow location access and try again.');
+        }
+      );
+
+
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+
+  }
+
+
+  // time zone conversion
+  // const timezoneOffset = 18000; // in seconds
+  // const localTime = new Date(new Date().getTime() + timezoneOffset * 1000);
+  // console.log(localTime.toLocaleString());
+
+
   return (
-    <div className=''>
+    <div className=' bg-blue-500 h-screen'>
       <div className="text-center">
-        <div className="text-5xl mb-5">Weather App</div>
+        <div className="text-5xl mb-5 text-white font-semibold pt-4">Weather App</div>
         <div>
           <input type="text"
             placeholder="Enter Your City Name"
             value={city}
             onChange={(e) => setCity(e.target.value)}
-            className="border-2 border-black rounded-lg"
+            onKeyDown={(e) => e.key === 'Enter' && featchWeather()}
+            className="border-2 border-white rounded-md outline-white"
           />
-          <button className="ml-4 bg-blue-600 text-white px-4 py-1 rounded-lg" onClick={featchWeather}>Enter</button>
+          <button className="ml-4 bg-blue-900 border border-white text-white px-4 py-1 rounded-lg" onClick={featchWeather}>Enter</button>
         </div>
         <div>
-          <div>data</div>
+          <div></div>
           {weather &&
-            <div>{weather.main.temp}</div>
+            <div className="text-white">
+              <div className="text-2xl font-medium my-2">{weather.name}</div>
+              <div className="text-3xl font-semibold">{weather.main.temp} °C</div>
+              <div className="flex gap-10 justify-center my-3">
+                <div><span className="font-semibold text-xl">Feels Like:</span> {weather.main.feels_like}</div>
+                <div><span className="font-semibold text-xl">Humidity:</span> {weather.main.humidity}</div>
+                <div>{new Date(weather.dt * 1000).toLocaleString()}</div>
+              </div>
+            </div>
           }
         </div>
+        <button className="border border-white" onClick={location}>location</button>
       </div>
     </div>
   );
+
+
+
 }
+
 
 export default App;
